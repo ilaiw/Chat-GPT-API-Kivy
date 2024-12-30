b9fc0f7ae5aaec7e4630b85306134fb32e91e632
ilai laptop <ilaiwai@gmail.com>
2024-12-30 15:49:55 +0200

    bat file

---
+++ ChatGPT.bat
@@ -0,0 +1,5 @@
+echo off
+cls
+call C:\Users\ilaiw\anaconda3\Scripts\activate.bat C:\Users\ilaiw\anaconda3
+start C:\Users\ilaiw\anaconda3\envs\mykivy\pythonw.exe C:\Users\ilaiw\PycharmProjects\Chat-GPT-API-Kivy\main.py
+exit
